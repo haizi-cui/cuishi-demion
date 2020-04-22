5008dd753d8f0a3b2271337f57bfefcc54d8a424
cxh <cxh18789401611@163.com>
2020-04-22 20:36:45 +0800

    ceshi

--- src/main/java/MySql/Jdbc/Select_Jdbc.java
+++ src/main/java/MySql/Jdbc/Select_Jdbc.java
@@ -15,7 +15,7 @@ public class Select_Jdbc {
         String sql = "select * from xing";
         Statement statement = connection.createStatement();
         //像数据库发送命令，增删改都用这个命令
-        statement.executeUpdate("insert into xing(age,namess,much,gnt_much,gmt_mucht) values ('"+people.getAge()+"','"+people.getNamess()+"','"+people.getMuch()+"','"+people.getGmt_much()+"','"+people.getGmt_mucht()+"')");
+        //statement.executeUpdate("insert into xing(age,namess,much,gnt_much,gmt_mucht) values ('"+people.getAge()+"','"+people.getNamess()+"','"+people.getMuch()+"','"+people.getGmt_much()+"','"+people.getGmt_mucht()+"')");
         }
 }
 
